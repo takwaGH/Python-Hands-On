9d49477f47efbbadf6bf92ad27b4a04a783145ee
takwaGH <123807794+takwaGH@users.noreply.github.com>
2023-02-04 10:38:43 +0100

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    if num <=1 :
+        return 1# Calculate factorial of a number using recursive function c
+    else:
+        return num * factorial(num-1)
+factorial(5)
+    
+  
+ 
+ 
+ 
+
