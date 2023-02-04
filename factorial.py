d43ae30eefd0f58643efe3453b55c38e99ea0378
takwaGH <123807794+takwaGH@users.noreply.github.com>
2023-02-04 11:50:08 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 def factorial(num):
     if num <=1 :
-        return 1# Calculate factorial of a number using recursive function c
+        return 1
     else:
         return num * factorial(num-1)
 factorial(5)
